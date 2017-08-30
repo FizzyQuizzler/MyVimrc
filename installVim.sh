10b5f91360d906c240de9ee12576444445ccdc09
fizzyquizzler <fizzyquizzlerart@gmail.com>
2017-08-29 22:12:16 -0400

    built a vim installation Script that will install vim and the vimrc for me.

---
+++ installVim.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#this script will handle installing all of the vim plugins that I use and the Powerline Terminal Tool.
+
+# this will handle installing git, vim, and tmux.
+echo "install git..."
+sudo apt-get -y install git vim tmux
+
+# this will handle creating the folder vim to work out of.
+echo "generating Folders for Vim..."
+mkdir ~/.vim
+mkdir ~/.vim/autoload
+mkdir ~/.vim/bundle
+
+# this will handle installing my vimrc.
+echo "download vimrc from github..."
+cd ~/.vim
+git clone https://github.com/FizzyQuizzler/MyVimrc.git
+cd ~/.vim/MyVimrc
+sudo chmod +x updateVimRC.sh
+./updateVimRC
+
+# this will handle installing all of the vim plugins.
+echo "installing Vim Plugins..."
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+# install auto pairs...
+git clone https://github.com/jiangmiao/auto-pairs.git
+# install java complete 2...
+git clone https://github.com/artur-shaik/vim-javacomplete2.git
+# install NerdTree...
+git clone https://github.com/scrooloose/nerdtree.git
+# install superTab...
+git clone https://github.com/ervandew/supertab.git
+# install Syntastic...
+git clone https://github.com/vim-syntastic/syntastic.git
+# install ALE.
+git clone https://github.com/w0rp/ale.git
+# install java unused imports.
+git clone https://github.com/akhaku/vim-java-unused-imports.git
+# install ctl-p
+$ cd ~/.vim
+$ git clone https://github.com/ctrlpvim/ctrlp.vim.git bundle/ctrlp.vim
+
+# install Powerline.
+echo "installing Powerline..."
+sudo apt-get install python3-pip
+pip3 install git+git://github.com/Lokaltog/powerline
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+fc-cache -vf /usr/share/fonts/
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+sudo apt-get -y install powerline
+
+# copying the tmux configuration file from Myvimrc.
+cp ~/.vim/MyVimrc/.tmux.conf ~/
+
+# do this Last ... 
+echo "Script Complete... Now Open vim and run this command ':helptags ~/.vim/bundle/ctrlp.vim/doc'"
+
+
+
+
+
